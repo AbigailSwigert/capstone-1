ff121253190b36fb04554d79bc3f5b6403076c79
Abigail Swigert <abigail@swigertdesigns.com>
2020-06-10 11:05:03 -0500

    cart items look good, item count still cannot be updated

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -11,13 +11,23 @@ export default function ShoppingCart({shoppingCart}) {
     }
 
     const cart = shoppingCart.map( item => {
+
+        const handleCountChange = e => item.count = e.target.value
+
         return(
-            <div className='cart-item'>
+            <div className='cart-item' key={item.id} >
                 <img src={process.env.PUBLIC_URL + item.img} alt={item.imgAlt} className='cart-item-img' />
                 <div className='item-info'>
                     <h3 className='cart-item-name' >{item.name}</h3>
                     <p className='cart-item-author'>{item.author}</p>
                     <p className='cart-item-stock'>{item.stock} left in stock</p>
+                </div>
+                <div className='count-n-price'>
+                    <p className='cart-item-price'>${item.price}</p>
+                    <button className='item-count-btn' id='decrement' onClick={() => item.count - 1} >-</button>
+                    <input className='single-item-count' value={item.count} onChange={handleCountChange} />
+                    <button className='item-count-btn' id='increment' onClick={() => item.count + 1} >+</button>
+                    <button className='remove-item'>Remove this Item</button>
                 </div>
             </div>
         )
